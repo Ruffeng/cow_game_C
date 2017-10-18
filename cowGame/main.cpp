3297bc07af90abf99c2beb097fd970daa4a3a1ef
Ruffeng <ruben.suet@gmail.com>
2017-10-18 18:33:06 +0200

    Initial Commit

---
+++ cowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cowGame
+//
+//  Created by Monkey on 18/10/17.
+//  Copyright © 2017 Monkey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
